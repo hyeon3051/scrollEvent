4926005dc2b3e16b222f3ef13ee9a5e81fc668c2
unknown <hyeon3051@gmail.com>
2020-08-25 15:43:31 +0900

    scroll Event

---
+++ scroll.js
@@ -0,0 +1,18 @@
+(function () {
+    const StageScroll = document.querySelector('.wrapper');
+    function onStageScroll(event) {
+        event.preventDefault();
+        let zMove = -500 + pageYOffset / (document.body.offsetHeight - window.innerHeight) * 1000;
+        console.log(zMove)
+        StageScroll.style.transform = `translateZ(${zMove}vw)`
+    }
+    function onMouseMove(event) {
+        mousePosx = -1 + (event.clientX / window.innerWidth) * 2;
+        mousePosy = 1 - (event.clientY / window.innerHeight) * 2;
+        console.log(mousePosx, mousePosy);
+        StageScroll.style.top=`${mousePosx}em`
+        StageScroll.style.left=`${mousePosy}em`
+    }
+    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener('scroll', onStageScroll);
+})();
